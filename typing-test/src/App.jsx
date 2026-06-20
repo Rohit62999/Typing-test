de6b4eec858ff1d296f978dac77d8b71b2f741ed
Rohit62999 <rohitkumar1368754@gmail.com>
2026-06-20 12:40:29 +0530

    Rechange of time

--- typing-test/src/App.jsx
+++ typing-test/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
     if (isActive && timeLeft > 0) {
       interval = setInterval(() => {
         setTimeLeft((time) => time - 1);
-      }, 100);
+      }, 1000);
     } else if (timeLeft === 0) {
       clearInterval(interval);
       setIsActive(false);
